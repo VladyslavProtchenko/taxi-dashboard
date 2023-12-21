2b297150265d49d8725d43404933aa56818721ca
Ihor Kachynskyi <ikachynskiy@gmail.com>
2023-12-21 09:27:11 +0200

    new commit

--- src/pages/Dashboard/Links/Confirm.tsx
+++ src/pages/Dashboard/Links/Confirm.tsx
@@ -10,7 +10,7 @@ const Confirm = ():React.ReactNode => {
     return (
         <div className='w-screen h-screen flex flex-col items-center justify-center'>
             <span className='mb-10 text-3xl'>You confirmed your trip! </span>
-            <NavLink to='/dashboard'  className='bg-sky-600 text-white px-6 py-3 rounded-full border-r-4 border-b-4 border-sky-800 active:border-0 active:mt-1'>Go to my profile</NavLink>
+            <NavLink to='/'  className='bg-sky-600 text-white px-6 py-3 rounded-full border-r-4 border-b-4 border-sky-800 active:border-0 active:mt-1'>Go to my profile</NavLink>
         </div>
     );
 };
--- src/pages/Dashboard/Links/Pending.tsx
+++ src/pages/Dashboard/Links/Pending.tsx
@@ -6,7 +6,7 @@ const Pending = ():React.ReactNode => {
     return (
         <div className='w-screen h-screen flex flex-col items-center justify-center'>
             <span className='mb-10 text-3xl'>Your trip is frozen !!! </span>
-            <NavLink to='/dashboard'  className='bg-sky-600 text-white px-6 py-3 rounded-full border-r-4 border-b-4 border-sky-800 active:border-0 active:mt-1'>Go to my profile!</NavLink>
+            <NavLink to='/'  className='bg-sky-600 text-white px-6 py-3 rounded-full border-r-4 border-b-4 border-sky-800 active:border-0 active:mt-1'>Go to my profile!</NavLink>
         </div>
     );
 };
